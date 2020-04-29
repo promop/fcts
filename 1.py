08662a46bdaa4e12c8090bb852e9992fac509591
romo <romopozapablo@gmail.com>
2020-04-29 10:28:25 +0200

    segundo comit

---
+++ 1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 11:03:02 2020
+
+@author: pablo
+"""
+
+print ("BIENVENIDO A EMPAREJANDO.COM")
+
+nombre= input("Tu nombre:")
+ano= int(input( "¿Año de nacimiento?" ))
+tegusta= input("¿Te gusta taburete?")
+
+edad  =  2020 - ano
+
+print ("hola", nombre, ". Si no me equivoco tienes", edad, "años.")
+
+if tegusta == "si" or tegusta == "Si":
+  print('OK Boomer, lo tuyo va a ser un caso difícil.')
+elif tegusta == "no" or tegusta == "No":
+  print('Bueno, al menos es un comienzo. Veremos qué se puede hacer contigo.')
+  
+  
---
+++ 3.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 15:16:02 2020
+
+@author: pablo
+"""
+print ("BIENVENIDO A EMPAREJANDO.COM")
+
+nombre= input("Tu nombre:")
+ano= int(input( "¿Año de nacimiento?" ))
+tegusta= input("¿Te gusta taburete?")
+
+edad  =  2020 - ano
+
+print ("hola", nombre, ". Si no me equivoco tienes", edad, "años.")
+
+if tegusta == "si" or tegusta == "Si":
+  print('OK Boomer, lo tuyo va a ser un caso difícil.')
+elif tegusta == "no" or tegusta == "No":
+  print('Bueno, al menos es un comienzo. Veremos qué se puede hacer contigo.')
+
+
+
+
+
+
+num=0
+
+
+for i in range(edad+1):
+
+    print("Que no cumpla",i)
+
+else:
+     print ("!!Que si cumpla",edad, )
+
